1144cbf03e2e71b8f38ec5d5bb7c8563f2359f7e
Ed Saa <esaavedrac@u.northwestern.edu>
2024-11-07 08:21:28 -0600

    Handle API change with streamlit 1.40

--- st_default_width.py
+++ st_default_width.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from functools import partial
 from types import MethodType
+from importlib.metadata import version
+
+STREAMLIT_VERSION = version("streamlit")
 
 CALLABLES_WITH_ADJUSTABLE_WIDTH = [
     "altair_chart",
@@ -49,7 +52,15 @@ def set_use_container_width_default(default: bool = True):
     """
 
     for func_name in CALLABLES_WITH_ADJUSTABLE_WIDTH:
-        kwarg = "use_column_width" if func_name == "image" else "use_container_width"
+        kwarg = "use_container_width"
+
+        ## API changed in version 1.40.0
+        # https://github.com/streamlit/streamlit/pull/9547
+        # Add use_container_width to st.image
+        if func_name == "image":
+            if int(STREAMLIT_VERSION.split(".")[1]) < 40:
+                kwarg = "use_column_width"
+
         func = st.__dict__[func_name]
 
         if isinstance(func, MethodType):
